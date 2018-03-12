6bf04643028a214cd95211f92dca3de8561b92b4
Anthony Mahadeo <anthony@financialapps.com>
2018-03-12 16:39:52 -0400

    port Campaign/CampaignFactory solidity contracts over from Remix editor

---
+++ ethereum/contracts/Campaign.sol
@@ -0,0 +1,84 @@
+pragma solidity ^0.4.17;
+
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    function Campaign(uint minimum, address creator) public {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+        require(msg.value > minimumContribution);
+
+        approvers[msg.sender] = true;
+        approversCount++;
+    }
+
+    function createRequest(string description, uint value, address recipient)
+        public restricted {
+            Request memory newRequest = Request({ // memory = copy of data, stored in memory, doesn't mutate original data; storage = points to instance state variable storage, changes to variable will be echoed in instance variable storage
+                description: description,
+                value: value,
+                recipient: recipient,
+                complete: false,
+                approvalCount: 0 // only have to initialize value types, not reference types -> which is why we don't touch approvers mapping here
+            });
+
+            // Request(description, value, recipient, false); this is alternative struct constructor syntax
+
+            requests.push(newRequest);
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index]; // using 'storage' because we want to manipulate actual request that exists in storage.
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount++;
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
+    }
+}
