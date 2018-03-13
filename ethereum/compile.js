b14787ed3d7501deae0dd1a3cd64857dfb96299f
Anthony Mahadeo <anthony@financialapps.com>
2018-03-13 11:52:29 -0400

    add solidity compile script

---
+++ ethereum/compile.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const solc = require("solc");
+const fs = require("fs-extra");
+
+const buildPath = path.resolve(__dirname, "build");
+fs.removeSync(buildPath); // remove existing build folder and contents
+
+const campaignPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+const source = fs.readFileSync(campaignPath, "utf8");
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath); // check to see if path exists, if not, will create it
+
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract + ".json"), // filename argument
+    output[contract] // data argument
+  );
+}
