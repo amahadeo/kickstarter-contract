3c9be546b8cb43e30c8ec7ba999ff77a51689af7
Anthony Mahadeo <anthonymahadeo@gmail.com>
2018-03-14 15:47:40 -0400

    add deployment script

---
+++ ethereum/deploy.js
@@ -0,0 +1,26 @@
+require("dotenv").load();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+  process.env.ACCT_NM,
+  process.env.RKBY_END
+);
+const web3 = new Web3(provider); // completely enabled for rinkeby (account info & network/node endpoint)
+
+const deploy = async () => {
+  // designed inside of function to be able to use async await syntax
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account ", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+};
+deploy();
