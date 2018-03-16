ef786b879e5ce1a9c84c2404b3f55647792127c5
Anthony Mahadeo <anthonymahadeo@gmail.com>
2018-03-15 21:44:56 -0400

    wip

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
+  static async getInitialProps() {
+    // this is a lifecycle method specific to next.js
+    // Next.js ignores all other lifecycle methods during server rendering
+    // need to include 'static' to define this method as a class method (next.js api)
+    // as Next tries to call this w/o having to render instance of component first
     const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-    console.log(campaigns);
+    return { campaigns }; // this object provided to component as props
   }
 
   render() {
-    return <div>Campaigns Index!</div>;
+    return <div>{this.props.campaigns[0]}</div>;
   }
 }
 
