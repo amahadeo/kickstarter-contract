faa999d46d7fae60539eac7855d96e9e945b0958
Anthony Mahadeo <anthony@financialapps.com>
2018-04-11 16:15:32 -0400

    change contract and redeploy

--- routes.js
+++ routes.js
@@ -1,3 +1,8 @@
 const routes = require("next-routes")();
 // use this file for dynamic routing
+
+routes
+  .add("/campaigns/new", "/campaigns/new") // need to add this before to prevent 'new' from being considered an 'address'
+  .add("/campaigns/:address", "/campaigns/show"); // route, component directory
+
 module.exports = routes;
