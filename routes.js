2f6ff6a638a796965ae659823e7e51cb82361fa8
Anthony Mahadeo <anthony@financialapps.com>
2018-04-12 16:58:36 -0400

    add request components'

--- routes.js
+++ routes.js
@@ -3,6 +3,8 @@ const routes = require("next-routes")();
 
 routes
   .add("/campaigns/new", "/campaigns/new") // need to add this before to prevent 'new' from being considered an 'address'
-  .add("/campaigns/:address", "/campaigns/show"); // route, component directory
+  .add("/campaigns/:address", "/campaigns/show") // route, component directory
+  .add("/campaigns/:address/requests", "/campaigns/requests/index")
+  .add("/campaigns/:address/requests/new", "/campaigns/requests/new");
 
 module.exports = routes;
